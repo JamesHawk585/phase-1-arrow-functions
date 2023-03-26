1a65484fdc6f658a4254d202106c571ce0ee4bd5
James Hawk <jph94880@gmail.com>
2023-03-26 16:54:27 -0500

    Adds my file.md

---
+++ index.js
@@ -0,0 +1,27 @@
+const divide = (num1, num2) => {
+    const quotient = num1 / num2;
+    return quotient;
+}
+
+const square = (num) => {
+    const square = num * num;
+    return square;
+}
+
+const add = (num1, num2) => {
+    const sum = num1 + num2;
+    return sum;
+}
+
+
+
+
+
+
+console.log(divide(10, 2));
+console.log(square(8));
+console.log(add(50, 36));
+
+
+
+
